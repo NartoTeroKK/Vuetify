0488f6fe95fe507ea1dcef2683d10c415b0885d9
NartoTeroKK <marcoperozzi00@gmail.com>
2021-11-09 13:51:35 +0100

    diocan

--- progetto_vue/src/main.js
+++ progetto_vue/src/main.js
@@ -1,15 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App)
-  .use(router)
-  .use(store)
-  .mount("#app");
-
-// I don't use it by default.
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount("#app");
+createApp(App).mount('#app')
