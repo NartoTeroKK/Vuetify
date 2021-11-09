fded604a7b39ce39bd6b0cee9a615f6fe4efcffb
NartoTeroKK <marcoperozzi00@gmail.com>
2021-11-09 13:37:33 +0100

    codio

--- progetto_vue/src/main.js
+++ progetto_vue/src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)
+  .use(store)
+  .mount("#app");
+
+// I don't use it by default.
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount("#app");
